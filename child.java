d4cc6b781fdd7340c7c7fe6b0584ba9c483f6956
Md Mazharul Haque <cpon143@gmail.com>
2023-02-01 10:36:30 +0530

    new chages

--- child.java
+++ child.java
@@ -31,19 +31,79 @@
 // }
 // }
 
-class Test {
-   final int a;
-//    final int a=20;
+// class Test {
+//    final int a;
+// //    final int a=20;
 
-//    {
-//     a=20;
-//    }
-Test (){a=25;}
+// //    { a=20;}
+// Test (){a=25;}
 
 
-public static void main(String[] args) {
-    Test t1 = new Test();
-    // t1.a = 20;
-    System.out.println(t1.a);
+// public static void main(String[] args) {
+//     Test t1 = new Test();
+//     // t1.a = 20;
+//     System.out.println(t1.a);
+// }
+// }
+
+
+// class Parent {
+//     Parent(int a) {
+//         System.out.println("Parent"+a);
+//     }
+// }
+
+// class Child extends Parent {
+//     Child() { super(7);
+//         System.out.println("Child");
+//     }
+// }
+
+// class Child1 extends Child {
+//     Child1() { 
+//         System.out.println("Child1");
+//     }
+//     public static void main(String [] args){
+//         Child1 c1 = new Child1();
+//     }
+// }
+
+
+// class Test{
+//     int a;
+//     int b;
+//     Test( int a, int b){
+//        this.a=a;
+//        this.b=b;
+//     }
+//     public static void main(String [] args){
+//         Test t1=new Test(3,4);
+//         Test t2=new Test(3,4);
+//         System.out.println(t1.a+" "+ t1.b);
+//         System.out.println(t2.a+" "+ t2.b);
+
+//     }
+// }
+
+
+class Test{
+    Test(){
+        this(10);
+        System.out.println("13t count");
+    }
+
+Test (int a){
+    this(7,5);
+    System.out.println("One Parameter");
 }
+    Test(int a, int b){
+        this("Hello");
+        System.out.ptintln("Two parameter const");
+    }
+    Test (String aa){
+        System.out.println("String constroctor");
+    }
+    public static void main(String [] args){
+        Test t1 = new Test();
+    }
 }
---
+++ constructir.java
@@ -0,0 +1,37 @@
+// class Constructor {
+//     public ThisDemo(){
+//         this(10); System.out.println("First constructor");
+//     }
+//     public ThisDemo(int a) //overload constructor
+//     {
+//         this(10,20); System.out.println("Second Constructor");
+//     }
+//     public ThisDemo(int a, int b); {//another overloaded constructor
+//     this("Ravi Kant"); System.out.println("Third Constructor");}
+//     public ThisDemo(String s) // and stil another
+//     {
+//         System.out.println("Fourth constructor");
+//     }
+//     public static void main(String [] args){
+//         ThisDemo d1=new ThisDemo();
+//     }
+// }
+// -----------------------------------------------------------------------
+// Chennig of the constructor or constructor overloading
+class ThisDemo {
+    public ThisDemo(){
+        this(10); System.out.println("First constructor");
+    }
+    public ThisDemo(int a) { //overload constructor
+        this(10,20); System.out.println("Second Constructor");
+    }
+    public ThisDemo(int a, int b) { //another overloaded constructor
+        this("Ravi Kant"); System.out.println("Third Constructor");
+    }
+    public ThisDemo(String s) { // and still another
+        System.out.println("Fourth constructor");
+    }
+    public static void main(String [] args){
+        ThisDemo d1=new ThisDemo();
+    }
+}
